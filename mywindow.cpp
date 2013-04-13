eaf17276478d2e842cda94c2d6aff5bcf1fac791
Cyril Leroux <cyril0leroux@gmail.com>
2013-04-14 00:05:20 +0200

    Connected button Signal to application slot

--- mywindow.cpp
+++ mywindow.cpp
@@ -14,5 +14,8 @@ MyWindow::MyWindow() : QWidget()
     m_button->setFont(QFont("Courier", 20, QFont::Bold, false));
     m_button->setCursor(Qt::PointingHandCursor);
 
-    m_button->move(60, 50);
+    m_button->move(160, 70);
+
+    // Connect m_button signal "clicked()" with Application slot "quit()"
+    QObject::connect(m_button, SIGNAL(clicked()), qApp, SLOT(quit()));
 }
